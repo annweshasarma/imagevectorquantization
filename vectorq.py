e4df78a3a92b2f0abc5e50252a0c85fad67cb8a7
annweshasarma <60536104+annweshasarma@users.noreply.github.com>
2020-02-02 10:19:37 +0530

    Update vectorq.py

--- vectorq.py
+++ vectorq.py
@@ -35,27 +35,3 @@ plt.imshow(face, cmap=plt.cm.gray, vmin=vmin, vmax=256)
 plt.figure(2, figsize=(3, 2.2))
 plt.imshow(face_compressed, cmap=plt.cm.gray, vmin=vmin, vmax=vmax)
 
-# equal bins face
-regular_values = np.linspace(0, 256, n_clusters + 1)
-regular_labels = np.searchsorted(regular_values, face) - 1
-regular_values = .5 * (regular_values[1:] + regular_values[:-1])  # mean
-regular_face = np.choose(regular_labels.ravel(), regular_values, mode="clip")
-regular_face.shape = face.shape
-plt.figure(3, figsize=(3, 2.2))
-plt.imshow(regular_face, cmap=plt.cm.gray, vmin=vmin, vmax=vmax)
-
-# histogram
-plt.figure(4, figsize=(3, 2.2))
-plt.clf()
-plt.axes([.01, .01, .98, .98])
-plt.hist(X, bins=256, color='.5', edgecolor='.5')
-plt.yticks(())
-plt.xticks(regular_values)
-values = np.sort(values)
-for center_1, center_2 in zip(values[:-1], values[1:]):
-    plt.axvline(.5 * (center_1 + center_2), color='b')
-
-for center_1, center_2 in zip(regular_values[:-1], regular_values[1:]):
-    plt.axvline(.5 * (center_1 + center_2), color='b', linestyle='--')
-
-plt.show()
